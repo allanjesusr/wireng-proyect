6b9b30566d4901cf79c3f0c95c5e0deb1a899fcd
Allan Rojas <allandechester@gmail.com>
2022-03-17 16:08:47 -0500

    6 commit

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -95,7 +95,7 @@ export const Footer = () => {
                 </div>
             </div>
 
-            <div className="footer__back-top">
+            <div className="footer__productsTitle">
                     <h2>The WirEng® 2022 Product Line</h2>
             </div>
 
@@ -248,24 +248,180 @@ export const Footer = () => {
                 <i className="fa-solid fa-caret-up navIcon"></i>
                 <b className="nav-b">Top of Page</b>
             </a>
-            <ul>
-                <li>Rural Solutions</li>
-                <li>Home Solutions</li>
-                <li>Mobile Solutions</li>
-                <li>Marine Solutions</li>
-                <li>Drones Solutions</li>
-                <li>Custom Solutions</li>
-            </ul>
-            <ul>
-                <li>Become a WirEng® System Installer</li>
-                <li>Become a WirEng® Representative</li>
-                <li>Become a WirEng® Reseller</li>
-                <li>Become a WirEng® Distributor</li>
-            </ul>
-            <div></div>
-            <div className="footer__rights-container">
-                <p>Copyright © WirEng®. All rights reserved.</p>
+
+            <div className="footer__mobileContent">
+                <div className="footer__mobileColumn">
+                    <ul className="footer__mobileList">
+                        <li>Rural Solutions</li>
+                        <li>Home Solutions</li>
+                        <li>Mobile Solutions</li>
+                        <li>Marine Solutions</li>
+                        <li>Drones Solutions</li>
+                        <li>Custom Solutions</li>
+                    </ul>
+                    <ul className="footer__mobileList">
+                        <li>Company Profile</li>
+                        <li>Testimonials</li>
+                        <li>Privacy Policy</li>
+                        <li>Terms and Conditions</li>
+                        <li>Site Map</li>
+                        <li>Website Terms of Service</li>
+                        <li>Shipping Policy</li>
+                        <li>Product Request</li>
+                        <li>Email Newsletter</li>
+                        <li>Technical Papers</li>
+                    </ul>
+                    
+                </div>
+                <div>
+                    <ul className="footer__mobileList">
+                        <li>Contact the WirEng® Engineering Team</li>
+                        <li>Contact the WirEng® Sales Team</li>
+                        <li>Contact the WirEng® Business Team</li>
+                        <li>Government/Military Relations</li>
+                        <li>B2B/OEM Relations</li>
+                        <li>B2B Partnerships</li>
+                    </ul>
+                    <ul className="footer__mobileList">
+                        <li>Become a WirEng® System Installer</li>
+                        <li>Become a WirEng® Representative</li>
+                        <li>Become a WirEng® Reseller</li>
+                        <li>Become a WirEng® Distributor</li>
+                    </ul>
+                </div>
+                <div className="footer_columntest">
+                    <ul className="footer__mobileList">
+                        <li>WirEng® in the World</li>
+                        <li>About WirEng®</li>
+                        <li>Contact Us</li>
+                    </ul>
+                    <ul className="footer__mobileList">
+                        <li>WirEng® Store</li>
+                        <li>Register Your WirEng® Product</li>
+                        <li>WirEng® Two-Year Warranty</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul className="footer__mobileList">
+                        <li>WirEng® on YouTube</li>
+                        <li>WirEng® on LinkedIn</li>
+                        <li>WirEng® on Tweeter</li>
+                        <li>WirEng® on Facebook</li>
+                        <li>WirEng® on Instagram</li>
+                        <li>WirEng® Careers</li>
+                    </ul>
+                    <ul className="footer__mobileList">
+                        <li>WirEng® Headquarters</li>
+                        <li>Austin, Texas 78729</li>
+                        <li>United States of America</li>
+                        <li>Tel. +1-443-353-9268</li>
+                    </ul>
+                </div>
             </div>
+
+            <div className="footer__mobileProduct-title">
+                <p>The WirEng® 2022 Product Line</p>
+            </div>
+
+            <div className="footer__mobileContent">
+                <div className="footer__mobileColumn">
+                    <ul className="footer__mobileList">
+                        <li className="footer__mobilelist-title">WideAnt-5G™ Series</li>
+
+                        <li>WideAnt-5G™</li>
+                        <li>WideAnt2-5G™</li>
+                        <li>WideAnt4-5G-4x2™</li>
+                        <li>WideAnt4-5G-4x4™</li>
+                        <li>WideAnt8-5G-8x2™</li>
+                        <li>WideAnt8-5G-8x4™</li>
+                        <li>WideAnt8-5G-8x8™</li>
+                    </ul>
+                    <ul className="footer__mobileList">
+                        <li className="footer__mobilelist-title">Omnirial-Plus-5G™ Series</li>
+
+                        <li>Omnirial2-Plus-5G™</li>
+                        <li>Omnirial4-Plus-5G-4x2™</li>
+                        <li>Omnirial4-Plus-5G-4x4™</li>
+                        <li>Omnirial8-Plus-5G-8x2™</li>
+                        <li>Omnirial8-Plus-5G-8x4™</li>
+                        <li>Omnirial8-Plus-5G-8x8™</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul className="footer__mobileList">
+                        <li className="footer__mobilelist-title">WideAnt-Plus-5G™ Series</li>
+
+                        <li>WideAnt-Plus-5G™</li>
+                        <li>WideAnt2-Plus-5G™</li>
+                        <li>WideAnt2-Plus-5G-Ref™</li>
+                        <li>WideAnt4-Plus-5G-4x2™</li>
+                        <li>WideAnt4-Plus-5G-4x4™</li>
+                        <li>WideAnt8-Plus-5G-8x2™</li>
+                        <li>WideAnt8-Plus-5G-8x4™</li>
+                        <li>WideAnt8-Plus-5G-8x8™</li>
+                    </ul>
+                    <ul className="footer__mobileList">
+                        <li className="footer__mobilelist-title">DroneAnt™ Series</li>
+
+                        <li>DroneAnt™</li>
+                        <li>DroneAnt-Plus™</li>
+                        <li>DroneAnt16™</li>
+                        <li>DroneAnt16-Plus™</li>
+                    </ul>
+                </div>
+                <div className="footer_columntest">
+                    <ul className="footer__mobileList">
+                        <li className="footer__mobilelist-title">WRG400-AU™ Series</li>
+
+                        <li>WRG400-SMAM-NM-10M-AU™</li>
+                        <li>WRG400-SMAM-NM-20M-AU™</li>
+                        <li>WRG400-SMAM-RA-NM-10M-AU™</li>
+                        <li>WRG400-SMAM-RA-NM-20M-AU™</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul className="footer__mobileList">
+                        <li className="footer__mobilelist-title">GigaMIMO-Plus-5G™ Series</li>
+
+                        <li>GigaMIMO-Plus-5G™</li>
+                        <li>GigaMIMO16-Plus-5G-2x2™</li>
+                        <li>GigaMIMO16-Plus-5G-4x2™</li>
+                        <li>GigaMIMO16-Plus-5G-4x4™</li>
+                    </ul>
+                    <ul className="footer__mobileList">
+                        <li className="footer__mobilelist-title">MIMOMount™ Series</li>
+
+                        <li>MIMOMount-2x2™</li>
+                        <li>MIMOMount-4x4™</li>
+                        <li>MIMOMount-8x8™</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul className="footer__mobileList">
+                        <li className="footer__mobilelist-title">ExtAnt-5G™ Series</li>
+
+                        <li>EXTANT-SMAF-SMAM™</li>
+                        <li>EXTANT-SMAF-TNCM™</li>
+                        <li>EXTANT-SMAF-TS9™</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul className="footer__mobileList">
+                        <li className="footer__mobilelist-title">OctAnt-5G™ Series</li>
+
+                        <li>OctAnt-5G-8x8™</li>
+                    </ul>
+                    <ul className="footer__mobileList">
+                        <li className="footer__mobilelist-title">GiAnt-5G™ Series</li>
+
+                        <li>GiAnt-5G™</li>
+                        <li>GiAnt2-5G™</li>
+                    </ul>
+                </div>
+            </div>
+                <div className="footer__mobilerights-container">
+                    <p>Copyright © WirEng®. All rights reserved.</p>
+                </div>
         </div>
         </footer>
         </>
